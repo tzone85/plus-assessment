699d28ff40bf5a1e4d11de09a3baa212e0346ba0
Thando Mini <tzone85@gmail.com>
2023-03-23 12:00:25 +0200

    finished layout configuration with some routing and using of outlets to display child components to routers

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,7 +1,10 @@
+import {Outlet} from "react-router-dom";
+
 export default function DefaultLayout() {
     return (
         <>
-
+            <h1>Default</h1>
+            <Outlet />
         </>
     )
 }
